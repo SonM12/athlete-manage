7dc12c6cac2b667afe2a1ca0625dd91ecadef5da
robuntu <robuntu@robuntu.ca>
2018-05-01 09:42:58 -0400

    Add Name class

---
+++ app/models/Name.java
@@ -0,0 +1,25 @@
+package models;
+
+import io.ebean.Finder;
+
+public class Name extends BaseModel{
+    public String firstName;
+    public String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstLast() {
+        return firstName + " " + lastName;
+    }
+
+    public String getLastFirst() {
+        return lastName + ", " + firstName;
+    }
+
+    public static Finder<Integer, Name> find = new Finder<>(Name.class);
+
+
+}
